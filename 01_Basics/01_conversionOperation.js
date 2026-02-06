2b399c7933017a68b5139af08ceca31a525be47a
Raj Mishra <raj.mishra.ds28@gmail.com>
2026-02-06 19:50:06 +0000

    Learn Basics

---
+++ 01_Basics/01_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "30abc"
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);   
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "" => False
+// "raj" => True
+
+
+// ******************** Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log("3"+4+8);
+// console.log(3+4+"8");
+
+// console.log(+true);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// 🧠 Memory trick:
+// x++ → “Use x, then add 1”
+// ++x → “Add 1, then use x”
---
+++ 01_Basics/02_comparison.js
@@ -0,0 +1,15 @@
+// Comparison should be doone between same data types..
+// IN js == and > works differently
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+console.log("2" === 2);  // === => check their datatypes!
+ 
