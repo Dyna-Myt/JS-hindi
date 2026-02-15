5ad1a827ccfc0e6f4e96e645363f21b44177682e
Raj Mishra <raj.mishra.ds28@gmail.com>
2026-02-15 06:36:07 +0000

    Scope

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,63 @@
+// {}  => Scope (When it comes with function or if-else)
+// things written inside {} is block scope
+// anything outside is global scope
+// never use "var" it causes life threatening issue
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){   // global scope
+    const username = "hitesh"
+
+    function two(){   // Block scope
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)  // here function can't access it 
+const addTwo = function(num){
+    return num + 2
+}
