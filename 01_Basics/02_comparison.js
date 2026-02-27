0e77d70c3e19b85aa74f8b0e0b1988700109cbb3
Raj Mishra <raj.mishra.ds28@gmail.com>
2026-02-27 08:21:19 +0000

    revision of comparison

--- 01_Basics/02_comparison.js
+++ 01_Basics/02_comparison.js
@@ -1,14 +1,14 @@
-// Comparison should be doone between same data types..
+// Comparison should be done between same data types..
 // IN js == and > works differently
 
 console.log(null > 0);
 console.log(null == 0);
-console.log(null >= 0);
+console.log(null >= 0);    // When using > JavaScript converts values to numbers => null → converted to number → 0
 
 
 console.log(undefined > 0);
 console.log(undefined == 0);
-console.log(undefined >= 0);
+console.log(undefined >= 0);   // Any comparison with NaN is always false.
 
 
 console.log("2" === 2);  // === => check their datatypes!
